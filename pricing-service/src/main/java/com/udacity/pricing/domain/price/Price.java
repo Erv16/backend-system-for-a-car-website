a07b61667c30f0150dd3e3e5723731dbf0224439
Erwin Palani <erwin.palani@gmail.com>
2022-07-03 19:29:05 -0500

    move from local to repo

---
+++ pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
@@ -0,0 +1,74 @@
+package com.udacity.pricing.domain.price;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+/**
+ * Represents the price of a given vehicle, including currency.
+ */
+
+@Entity
+public class Price {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "currency")
+    private String currency;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Column(name = "vehicleid")
+    private Long vehicleId;
+
+    public Price() {
+    }
+
+    public Price(String currency, BigDecimal price, Long vehicleId) {
+        this.currency = currency;
+        this.price = price;
+        this.vehicleId = vehicleId;
+    }
+
+    public Price(Long id, String currency, BigDecimal price, Long vehicleId) {
+        this.id = id;
+        this.currency = currency;
+        this.price = price;
+        this.vehicleId = vehicleId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Long getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(Long vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+}
---
+++ pricing-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO price (id, currency, price, vehicleid) VALUES(1, 'USD', 10000, 1);
+INSERT INTO price (id, currency, price, vehicleid) VALUES(2, 'USD', 20000, 2);
+INSERT INTO price (id, currency, price, vehicleid) VALUES(3, 'USD', 12000, 3);
+INSERT INTO price (id, currency, price, vehicleid) VALUES(4, 'USD', 22000, 4);
+INSERT INTO price (id, currency, price, vehicleid) VALUES(5, 'USD', 34000, 5);
